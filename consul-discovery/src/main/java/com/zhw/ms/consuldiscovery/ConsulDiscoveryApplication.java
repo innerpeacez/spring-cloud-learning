fdc0e60298cec1cc5ddd65e74f009c3825d843ff
innerpeacez <zhaihw0417@163.com>
2018-12-12 21:28:25 +0800

    spring modulus 聚合

---
+++ consul-discovery/src/main/java/com/zhw/ms/consuldiscovery/ConsulDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.zhw.ms.consuldiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulDiscoveryApplication.class, args);
+    }
+}
